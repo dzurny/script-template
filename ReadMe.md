eca840cdb1f3824b5697de5bf20815239ebd1f08
George <kanzilbjrj@gmail.com>
2025-07-12 11:12:05 +0200

    Add files via upload

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+## Basic FiveM script template. :D ##
---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+-- FX Version
+fx_version "cerulean"
+game "gta5
+-- Script Version
+version "1.0.0"
+-- Author Info
+author "Your Name, džurny"
+description "Script Template"
+-- Client Scripts
+client_scripts {
+    "client/client.lua"
+}
+-- Server Scripts
+server_scripts {
+    "server/server.lua"
+}
+-- Additional Files
+files {
+    "config.lua",
+    "locales/*.json"
+}
